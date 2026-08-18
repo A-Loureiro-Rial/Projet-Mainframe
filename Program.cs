b9b5785354bb20c630d969c5f3e3ce795febd0a8
A-Loureiro-Rial <alice.loureiro.rial@gmail.com>
2026-08-18 07:04:49 +0200

    external API call functionnal

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DotEnv.Core;
+using Newtonsoft.Json;
+using System;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace ProjetFin
+{
+    internal class Program
+    {
+        internal static async Task Main()
+        {
+            Console.WriteLine("Before call");
+            Rates test = new Rates();
+            Console.WriteLine("After call");
+
+        }
+    }
+}
